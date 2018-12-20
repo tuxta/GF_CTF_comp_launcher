a4b50176475ca1df25101311f05fe7170bdba652
Tuxta <tuxta2@gmail.com>
2018-12-20 17:29:53 +1000

    Set teams to passed arguments

--- Battles/MainController.py
+++ Battles/MainController.py
@@ -12,8 +12,6 @@ pygame.mouse.set_visible(False)
 
 pygame.display.set_caption(Globals.window_name)
 window_size = (Globals.SCREEN_WIDTH, Globals.SCREEN_HEIGHT)
-# screen = pygame.display.set_mode(window_size,
-#                                 pygame.DOUBLEBUF, 32)
 
 screen = pygame.display.set_mode(window_size,
                                  pygame.FULLSCREEN)
@@ -21,10 +19,8 @@ screen = pygame.display.set_mode(window_size,
 Globals.next_level = Globals.start_level
 levels = Globals.levels
 
-Globals.red_player = 'Nerang'
-Globals.blue_player = 'Varsity'
-#Globals.red_player = sys.argv[1]
-#Globals.blue_player = sys.argv[2]
+Globals.red_player = sys.argv[1]
+Globals.blue_player = sys.argv[2]
 
 print(Globals.blue_player)
 print(Globals.red_player)
